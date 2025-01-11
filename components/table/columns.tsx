5eae6d5f64c69a8825e6051d962bce9ab2e943d3
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2025-01-11 12:20:06 +0530

    App Fixed

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -1,69 +1,56 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import Image from "next/image";
-
-import { Doctors } from "@/constants";
-import { formatDateTime } from "@/lib/utils";
-import { Appointment } from "@/types/appwrite.types";
-
-import { AppointmentModal } from "../AppointmentModal";
 import { StatusBadge } from "../StatusBadge";
+import { formatDateTime } from "@/lib/utils";
+import { Doctors } from "@/constants";
+import Image from "next/image";
+import { AppointmentModal } from "../AppointmentModal";
+import { Appointment } from "@/types/appwrite.types";
 
 export const columns: ColumnDef<Appointment>[] = [
   {
-    header: "#",
-    cell: ({ row }) => {
-      return <p className="text-14-medium ">{row.index + 1}</p>;
-    },
+    header: "ID",
+    cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
   },
   {
     accessorKey: "patient",
     header: "Patient",
-    cell: ({ row }) => {
-      const appointment = row.original;
-      return <p className="text-14-medium ">{appointment.patient.name}</p>;
-    },
+    cell: ({ row }) => (
+      <p className="text-14-medium">{row?.original?.patient?.name}</p>
+    ),
   },
   {
     accessorKey: "status",
     header: "Status",
-    cell: ({ row }) => {
-      const appointment = row.original;
-      return (
-        <div className="min-w-[115px]">
-          <StatusBadge status={appointment.status} />
-        </div>
-      );
-    },
+    cell: ({ row }) => (
+      <div className="min-w-[115px]">
+        <StatusBadge status={row.original.status} />
+      </div>
+    ),
   },
   {
     accessorKey: "schedule",
     header: "Appointment",
-    cell: ({ row }) => {
-      const appointment = row.original;
-      return (
-        <p className="text-14-regular min-w-[100px]">
-          {formatDateTime(appointment.schedule).dateTime}
-        </p>
-      );
-    },
+    cell: ({ row }) => (
+      <p className="text-14-regular min-w-[100px]">
+        {formatDateTime(row.original.schedule).dateTime}
+      </p>
+    ),
   },
   {
     accessorKey: "primaryPhysician",
-    header: "Doctor",
+    header: () => "doctor",
     cell: ({ row }) => {
-      const appointment = row.original;
-
       const doctor = Doctors.find(
-        (doctor) => doctor.name === appointment.primaryPhysician
+        (doc) => doc.name === row.original.primaryPhysician
       );
 
       return (
         <div className="flex items-center gap-3">
           <Image
             src={doctor?.image!}
-            alt="doctor"
+            alt={doctor?.name || "doctor"}
             width={100}
             height={100}
             className="size-8"
@@ -76,26 +63,24 @@ export const columns: ColumnDef<Appointment>[] = [
   {
     id: "actions",
     header: () => <div className="pl-4">Actions</div>,
-    cell: ({ row }) => {
-      const appointment = row.original;
-
+    cell: ({ row: { original: data } }) => {
       return (
         <div className="flex gap-1">
           <AppointmentModal
-            patientId={appointment.patient.$id}
-            userId={appointment.userId}
-            appointment={appointment}
             type="schedule"
+            patientId={data?.patient?.$id}
+            userId={data.userId}
+            appointment={data}
             title="Schedule Appointment"
-            description="Please confirm the following details to schedule."
+            description="Schedule a new appointment"
           />
           <AppointmentModal
-            patientId={appointment.patient.$id}
-            userId={appointment.userId}
-            appointment={appointment}
             type="cancel"
+            patientId={data?.patient?.$id}
+            userId={data.userId}
+            appointment={data}
             title="Cancel Appointment"
-            description="Are you sure you want to cancel your appointment?"
+            description="Cancel the existing appointment"
           />
         </div>
       );
