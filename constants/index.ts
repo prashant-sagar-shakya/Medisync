f163376e3f79d94ce20b86f7737753fe6dd17cce
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2025-01-11 15:00:48 +0530

    Updated

--- constants/index.ts
+++ constants/index.ts
@@ -27,15 +27,11 @@ export const PatientFormDefaultValues = {
 };
 
 export const IdentificationTypes = [
+  "Aadhar Card",
   "Birth Certificate",
   "Driver's License",
   "Medical Insurance Card/Policy",
-  "Military ID Card",
-  "National Identity Card",
   "Passport",
-  "Resident Alien Card (Green Card)",
-  "Social Security Card",
-  "State ID Card",
   "Student ID Card",
   "Voter ID Card",
 ];
